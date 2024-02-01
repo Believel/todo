f54b402bc072f57fc3cb934935c5ef468035f8d4
believel <zjrzpp0414@163.com>
2024-02-01 10:06:49 +0800

    todo update

--- my-react-todo/src/components/Input.jsx
+++ my-react-todo/src/components/Input.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react'
-import { Input } from 'antd'
+import { Input, Space, Button } from 'antd'
 import { useDispatch } from 'react-redux'
 import { addTodo } from '../store/todosSlice'
 export default function InputComponent() {
   const [text, setText] = useState('')
   const dispatch = useDispatch()
 
-  const handleSubmit = (e) => {
-    const text = e.target.value.trim()
-    if (e.which === 13 ) {
-      dispatch(addTodo(text))
-    }
+  const add = () => {
+    dispatch(addTodo(text))
     setText('')
   }
 
   return (
-    <Input placeholder='What needs to be done?' onKeyDown={handleSubmit} value={text} onChange={e => setText(e.target.value)} />
+    <Space.Compact style={{ width: '100%' }}>
+      <Input
+      placeholder='What needs to be done?'
+      value={text}
+      onChange={e => setText(e.target.value)}
+      allowClear />
+      <Button type="primary" onClick={add}>新增</Button>
+    </Space.Compact>
   )
 }
